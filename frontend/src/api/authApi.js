5ea1ed17c95580840165032f8c0f82e262228b5e
paraj <bhattparaj2@gmail.com>
2024-08-05 12:00:19 +0530

    correct authapi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -5,7 +5,7 @@ const AUTH_URL = import.meta.env.VITE_API_URL;
 
 export const login = async (credentials) => {
   try {
-    const response = await axios.post(`${AUTH_URL}/login`, credentials);
+    const response = await axios.post(`${AUTH_URL}/api/auth/login`, credentials);
     return response.data;
   } catch (error) {
     console.error('Login error:', error.response ? error.response.data : error.message);
@@ -16,7 +16,7 @@ export const login = async (credentials) => {
 export const signup = async (userData) => {
   try {
     console.log("I am inside api calling");
-    const response = await axios.post(`${AUTH_URL}/signup`, userData);
+    const response = await axios.post(`${AUTH_URL}/api/auth/signup`, userData);
     return response.data;
   } catch (error) {
     console.error('Signup error:', error.response ? error.response.data : error.message);
